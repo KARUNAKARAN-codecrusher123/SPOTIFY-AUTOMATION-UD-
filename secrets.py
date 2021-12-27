f97badef8a2af1f5c44b9c4fc58355ff5eb338cf
KARUNAKARAN V VIJAYARAGHAVAN <66834895+karunakaran186@users.noreply.github.com>
2021-12-28 01:47:11 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,2 +1,2 @@
-spotify_token = "BQAx92fBqkpdAcxwjWeKkfqWM0FjSdvFWKzM2DoN9suAHOLBShdPE0sljA2LsSjrC8g9ctrle9TQyODGfxccfrec_Kb4qTkO2ImQggd7geSxhUmnIKGmv23kfYheX9WaE3W2E_1QeklRcwFKEuddpTY6_jetUO8o8h6dcc02akDHXU4JYK7D87tJDGB_jPVkVD1i1A"
-spotify_user_id = "317innuenhb6bz7qqt6mebnw4vzq"
+spotify_token = ""
+spotify_user_id = ""
